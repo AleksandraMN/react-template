b1e6a72602b959c1b698b170c82e79a1835ea20c
AleksandraMN <al_mn@mail.ru>
2024-08-27 23:49:31 +1000

    react project

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import top from './img/photo.jpeg';
+
+const Module = () => (
+   <>
+     <h1>Hello, React!</h1>
+     <img width={500} src={top} />
+   </>
+);
+
+export default Module;
